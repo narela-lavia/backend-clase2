1d815046be6f6abc615bd431dcbcf241e597e45c
Narela Lavia <lavia.narela@gmail.com>
2023-02-09 20:49:52 -0300

    creacion del servidor

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const PORT = 3000;
+
+app.listen(PORT, () =>
+    console.log('listening on port', PORT));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productos.html'));
+});
+
+app.get('/productos/1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/producto1.html'));
+});
